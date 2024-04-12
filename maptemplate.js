16e564361cc0e8c35c39508126c09a014fd0eef0
Michael Kollasch <mrkollasch@comcast.net>
2024-04-12 09:36:05 -0500

    working 2014 and 2015 map files

---
+++ maptemplate.js
@@ -0,0 +1,86 @@
+// read in the data for the buildings
+d3.json("benchmarking.json").then(function (response) {
+
+    // Build 2014 array
+    let buildings2014 = [];
+
+    //Filter out only rows with DataYear = 2014            Buildings had one year to comply
+    var filtered2014 = response.filter(a => a.DataYear == 2014);
+
+    // Loop through the whole array and filter out the latitude and longitude
+    for (let index = 0; index < filtered2014.length; index++) {
+        let location = filtered2014[index];
+
+        // For each building, create a marker, and bind a popup. For now - its the name and address
+        let building2014 = L.marker([location.Latitude, location.Longitude])
+            .bindPopup("<h3>" + location.PropertyName + "<h3><h3>Address: " + location.Address + "</h3>");
+
+        // Add the marker to the buildings2014 array
+        buildings2014.push(building2014);
+    }
+    // Create a LAYER GROUP with the buildings2014 array
+    var fourteen = L.layerGroup(buildings2014);
+
+//  2014 data and markers and layer group prepared!!!!
+
+
+    // Create the tile layer that will be the background of our map.
+    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+
+    // Create a baseMaps object to hold the streetmap layer.
+    let baseMaps = {
+        "Street Map": streetmap
+    };
+
+    // Create an overlayMaps object to hold the 2014 (fourteen) layer.
+    let overlayMaps = {
+        "2014": fourteen
+    };
+
+    // Create the map object with options.
+    let map = L.map("map-id", {
+        center: [41.88, -87.6428],
+        zoom: 15,
+        layers: [streetmap, fourteen]
+    });
+
+    // The map has now been ADDED
+
+
+
+    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+    var layerControl = L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+
+
+
+    // Build 2015 array
+    let buildings2015 = [];
+//Filter out only rows with DataYear = 2015 
+    var filtered2015 = response.filter(a => a.DataYear == 2015);
+
+    // Loop through the 2015 array.
+    for (let index = 0; index < filtered2015.length; index++) {
+        let location = filtered2015[index];
+
+        // For each building, create a marker, and bind a popup 
+        let building2015 = L.marker([location.Latitude, location.Longitude])
+        .bindPopup("<h3>" + location.PropertyName + "<h3><h3>Address: " + location.Address + "</h3>");
+
+        // Add the marker to the 2015 array.
+        buildings2015.push(building2015);
+         // Create a LAYER GROUP with the buildings2015 array
+        var fifteen = L.layerGroup(buildings2015);
+
+    };
+
+
+
+    layerControl.addOverlay(fifteen, "2015");
+
+
+})
